89aeff8ec3dbd3932365263d0118469c7dc8a942
Abdelbasset JARRAY <jarrayabdelbasset@gmail.com>
2020-04-21 19:02:45 +0100

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -7,23 +7,23 @@ export OKD_VERSION=3.11
 
 #OKD Master Node Configuration
 export OKD_MASTER_IP=100.10.33.100
-export OKD_MASTER_HOSTNAME=okd-master-node
+export OKD_MASTER_HOSTNAME=okdmaster-node
 
 #OKD Worker Node 1 Configuration
 export OKD_WORKER_NODE_1_IP=100.10.33.101
-export OKD_WORKER_NODE_1_HOSTNAME=okd-worker1-node
+export OKD_WORKER_NODE_1_HOSTNAME=okdworker1-node
 
 #OKD Worker Node 2 Configuration
 export OKD_WORKER_NODE_2_IP=100.10.33.102
-export OKD_WORKER_NODE_2_HOSTNAME=okd-worker2-node
+export OKD_WORKER_NODE_2_HOSTNAME=okdworker2-node
 
 #OKD Worker Node 3 Configuration
 export OKD_WORKER_NODE_3_IP=100.10.33.103
-export OKD_WORKER_NODE_3_HOSTNAME=okd-worker3-node
+export OKD_WORKER_NODE_3_HOSTNAME=okdworker3-node
 
 #OKD Infra Node 1 Configuration
 export OKD_INFRA_NODE_1_IP=100.10.33.104
-export OKD_INFRA_NODE_1_HOSTNAME=okd-infra-node
+export OKD_INFRA_NODE_1_HOSTNAME=okdinfra-node
 
 #The  below setting will be used to access OKD console https://console.$DOMAIN:$API_PORT"
 #By default we can login using the URL https://console.okd.abdoujarray.com:8443
